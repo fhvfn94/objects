ad438740e9a488e1429e94d4a3a8a84d83e6eaa7
Eldar <fhvfn94@gmail.com>
2022-11-17 21:39:04 +0300

    сдÐелал задание проделение строки на пополам

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,20 @@
 public class Main {
+    public static String[] strDelenie(String a) {
+        String[] r = new String[2];
+        if (a.length() % 2 == 0) {
+            r[0] = a.substring(0, a.length() / 2);
+            r[1] = a.substring(a.length() / 2);
+        } else {
+            r[0] = a.substring(0, a.length() / 2);
+            r[1] = a.substring(a.length() / 2 + 1);
+        }
+        return r;
+    }
     public static void main(String[] args) {
-        Author author1 = new Author("adsdsd", "fdfd");
-        Book book1 = new Book("Qara so'zderi", 1904, author1);
-        System.out.println(book1.toString());
-        System.out.println(author1.toString());
+        //Напишите программу, которая разделяет любую строку на две части ровно пополам по количеству символов.
+        String a = "abvg";
+        String[] srtDelenie = strDelenie(a);
+        System.out.println(srtDelenie[0] + "/" + srtDelenie[1]);
+
     }
 }
